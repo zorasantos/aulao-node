eefa327225148ad5152c808aff8127a89a28fa79
Zoranildo Santos <zoranildo.santos@pge.ce.gov.br>
2021-10-02 17:25:21 -0300

    Aulão node 02

--- src/server.js
+++ src/server.js
@@ -7,12 +7,45 @@ const port = '3000'
 
 app.use(express.json())
 
+/**
+ * POST
+ * GET
+ * PUT
+ * DELETE
+ */
+
 app.post('/create', async (req, res) => {
-  const { medic_name, exam_request, hospital_medic_requesting, date, note } = req.body
+  const { medic_name, exam_request, hospital_medic_requesting, date, note } = req.body // Capturando os dodos vindos da requisição
   await db('exam').insert({
     medic_name, exam_request, hospital_medic_requesting, date, note
-  })
-  return res.status(200).send({ msg: 'Exame cadastrado com sucesso' })
+  })  // Inserindo no banco de dados na tabela exam os dodos capturados na requisição
+  return res.status(200).send({ msg: 'Exame cadastrado com sucesso' }) // Retorna uma mensagem de sucesso
+})
+
+//async/await é um método para resolver uma promisse
+
+app.get('/list_exams', async (req, res) => {
+  const exams = await db('exam').select('*')
+  return res.json(exams)
+})
+
+app.put('/update/:id', async (req, res) => {
+  const data = req.body
+  const { id } = req.params
+
+  await db('exam').where({ id }).update(data)
+
+  return res.status(200).send({ msg: 'Exame atualizado com sucesso!' })
+
+})
+
+app.delete('/remove/:id', async (req, res) => {
+  const { id } = req.params
+
+  await db('exam').where({ id }).first().delete()
+
+  return res.status(200).send({ msg: `Exame de id: ${id}, excluído com sucesso!` })
+
 })
 
 app.listen(port, () => {
